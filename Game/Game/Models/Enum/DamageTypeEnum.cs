bc78817f2084faec7a87c2931e7b1ca42e37c43f
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-01-30 17:23:04 -0800

    Added proper documentation to DamageTypeEnum.cs

--- Game/Game/Models/Enum/DamageTypeEnum.cs
+++ Game/Game/Models/Enum/DamageTypeEnum.cs
@@ -4,11 +4,22 @@ using System.Text;
 
 namespace Game.Models
 {
+    /// <summary>
+    /// The elemental types of damage
+    /// Used by DamageInfo to specify what element of damage is being dealt
+    /// </summary>
     public enum DamgeTypeEnum
     {
+        //No element
         None = 0,
+
+        //Fire damage
         Fire = 1,
+
+        //Electric damage
         Electric = 2,
+
+        //Poison damage
         Poison = 3
     }
 }
