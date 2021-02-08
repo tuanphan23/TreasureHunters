746ac41c0fcfdc33f4b68b8aed6dbf90ab9a5311
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-02-08 03:23:48 -0800

    Added FlexBox to the CharacterReadPage.xaml and the corresponding functions to impliment it in CharacterReadPage.xaml.cs

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms.Xaml;
 
 using Game.ViewModels;
 using Game.Models;
+using System.Linq;
 
 namespace Game.Views
 {
@@ -34,6 +35,7 @@ namespace Game.Views
             BindingContext = this.ViewModel = data;
             this.ViewModel.Title = "Read Character";
 
+            AddItemsToDisplay();
         }
 
         /// <summary>
@@ -56,6 +58,85 @@ namespace Game.Views
         {
             await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(ViewModel)));
             await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Adds the items to the flexbox
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
+        }
+
+        /// <summary>
+        /// Creates a stack item to return to the flexbox
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns>ItemStack to display</returns>
+        private View GetItemToDisplay(ItemLocationEnum location)
+        {
+            //Default Image is the plus
+            var ImageSource = "icon_cancel.png";
+            var ClickableButton = true;
+
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                //Show default icon
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
+
+                //Turn off Clickable action
+                ClickableButton = false;
+            }
+
+            //Hookup image button to show item pic
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            if (ClickableButton)
+            {
+                //Add Event so user can click to see more about item
+                //ItemButton.Clicked += (sender, args) => ShowPopup(data);
+            }
+
+            //Add displayText
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            //Put Image button and text in layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+
         }
     }
 }
