c93ed2a5a9e7bd33fd35f7c522732aa09392709d
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-02-08 13:59:01 -0800

    Changed all instances of CharacterModel to MonsterModel in MonsterDeletePage.xaml.cs re-enabled delet epage navigation in MonsterReadPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -58,7 +58,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Delete_Clicked(object sender, EventArgs e)
         {
-            //await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(ViewModel)));
+            await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(ViewModel)));
             await Navigation.PopAsync();
         }
 
