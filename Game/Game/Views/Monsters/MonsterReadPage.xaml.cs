2d2372e860532103942b08e6dc4650982eb8e08f
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-02-08 13:44:30 -0800

    Replaced all instances of CharacterModel with MonsterModel in MonsterReadPage.xaml enabled the navigation to read page. Removed navigation to the MonsterUpdatePage and MonsterDeletePage until constructor types are fixed

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -18,7 +18,7 @@ namespace Game.Views
     public partial class MonsterReadPage : ContentPage
     {
         // View Model for Item
-        public readonly GenericViewModel<CharacterModel> ViewModel;
+        public readonly GenericViewModel<MonsterModel> ViewModel;
 
         // Empty Constructor for UTs
         public MonsterReadPage(bool UnitTest) { }
@@ -29,7 +29,7 @@ namespace Game.Views
         /// The viewModel is the data that should be displayed
         /// </summary>
         /// <param name="viewModel"></param>
-        public MonsterReadPage(GenericViewModel<CharacterModel> data)
+        public MonsterReadPage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
 
@@ -47,7 +47,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Update_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new MonsterUpdatePage(ViewModel)));
+            //await Navigation.PushModalAsync(new NavigationPage(new MonsterUpdatePage(ViewModel)));
             await Navigation.PopAsync();
         }
 
@@ -58,7 +58,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Delete_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(ViewModel)));
+            //await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(ViewModel)));
             await Navigation.PopAsync();
         }
 
