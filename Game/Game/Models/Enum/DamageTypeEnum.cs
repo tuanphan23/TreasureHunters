d92ebc54735508b6e4d1f6783aecdd047bf9b4a3
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-02-11 20:56:34 -0800

    Added helper classes and methods for DamageTypeEnum.cs

--- Game/Game/Models/Enum/DamageTypeEnum.cs
+++ Game/Game/Models/Enum/DamageTypeEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Game.Models
@@ -24,5 +25,78 @@ namespace Game.Models
 
         //Healing rather than damage
         Heal = 10
+    }
+
+
+    /// <summary>
+    /// Friendly strings for the Enum Class
+    /// </summary>
+    public static class DamageTypeEnumExtensions
+    {
+        /// <summary>
+        /// Display a String for the Enums
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToMessage(this DamageTypeEnum value)
+        {
+            // Default String
+            var Message = "None";
+
+            switch (value)
+            {
+                case DamageTypeEnum.None:
+                    Message = "None";
+                    break;
+
+                case DamageTypeEnum.Fire:
+                    Message = "Fire";
+                    break;
+
+                case DamageTypeEnum.Electric:
+                    Message = "Electric";
+                    break;
+
+                case DamageTypeEnum.Poison:
+                    Message = "Poison";
+                    break;
+
+                case DamageTypeEnum.Heal:
+                    Message = "Healing";
+                    break;
+            }
+
+            return Message;
+
+            return Message;
+        }
+    }
+
+    /// <summary>
+    /// Helper for the DamageTypeEnum Class
+    /// </summary>
+    public static class DamageTypeEnumHelper
+    {
+        /// <summary>
+        /// Returns a list of strings of the enum for DamageType
+        /// </summary>
+        public static List<string> GetFullList
+        {
+            get
+            {
+                var myList = Enum.GetNames(typeof(DamageTypeEnum)).ToList();
+                return myList;
+            }
+        }
+
+        /// <summary>
+        /// Given the String for an enum, return its value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static CharacterJobEnum ConvertStringToEnum(string value)
+        {
+            return (CharacterJobEnum)Enum.Parse(typeof(CharacterJobEnum), value);
+        }
     }
 }
