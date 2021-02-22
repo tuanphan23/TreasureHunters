fa1cc9107dbb36e71fa0e985b7d65bc5977698ae
Tuan Phan <tphan@seattleu.edu>
2021-02-22 00:57:27 -0800

    Updated more fields for Create Page of Monster

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -7,6 +7,8 @@ using Xamarin.Forms.Xaml;
 using Game.Models;
 using Game.ViewModels;
 using Game.GameRules;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Game.Views
 {
@@ -18,6 +20,12 @@ namespace Game.Views
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
     public partial class MonsterCreatePage : ContentPage
     {
+        // maximum value for monster's attribute
+        public int MaxAttributeValue = 10;
+
+        // maximum value for monster's attribute
+        public int MinAttributeValue = 0;
+
         // The Character to create
         public GenericViewModel<MonsterModel> ViewModel = new GenericViewModel<MonsterModel>();
 
@@ -38,6 +46,33 @@ namespace Game.Views
             this.ViewModel.Title = this.ViewModel.Data.Name;
 
             BindingContext = ViewModel;
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Redo the Binding to cause a refresh
+        /// </summary>
+        /// <returns></returns>
+        public bool UpdatePageBindingContext()
+        {
+            // Temp store off the Level
+            var data = this.ViewModel.Data;
+
+            // Clear the Binding and reset it
+            BindingContext = null;
+            this.ViewModel.Data = data;
+            this.ViewModel.Title = data.Name;
+
+            BindingContext = this.ViewModel;
+
+            DifficultyPicker.SelectedItem = ViewModel.Data.Difficulty.ToString();
+
+            AddItemsToDisplay();
+
+            SetEnableStateAttributeButtons();
+
+            return true;
         }
 
         /// <summary>
@@ -81,14 +116,290 @@ namespace Game.Views
         /// <param name="e"></param>
         public void RandomButton_Clicked(object sender, EventArgs e)
         {
-            BindingContext = null;
+            this.ViewModel.Data.Update(RandomPlayerHelper.GetRandomMonster(1));
 
-            this.ViewModel.Data = RandomPlayerHelper.GetRandomMonster(1);
-            this.ViewModel.Title = this.ViewModel.Data.Name;
-
-            BindingContext = ViewModel;
+            UpdatePageBindingContext();
 
             return;
+        }
+
+        /// <summary>
+        /// Walk each button and set the enabled to true or false
+        /// </summary>
+        /// <returns></returns>
+        public bool SetEnableStateAttributeButtons()
+        {
+            AttackUpButton.IsEnabled = true;
+            if (ViewModel.Data.Attack == MaxAttributeValue)
+            {
+                AttackUpButton.IsEnabled = false;
+            }
+
+            AttackDownButton.IsEnabled = true;
+            if (ViewModel.Data.Attack == MinAttributeValue)
+            {
+                AttackDownButton.IsEnabled = false;
+            }
+
+            DefenseUpButton.IsEnabled = true;
+            if (ViewModel.Data.Defense == MaxAttributeValue)
+            {
+                DefenseUpButton.IsEnabled = false;
+            }
+
+            DefenseDownButton.IsEnabled = true;
+            if (ViewModel.Data.Defense == MinAttributeValue)
+            {
+                DefenseDownButton.IsEnabled = false;
+            }
+
+            SpeedUpButton.IsEnabled = true;
+            if (ViewModel.Data.Speed == MaxAttributeValue)
+            {
+                SpeedUpButton.IsEnabled = false;
+            }
+
+            SpeedDownButton.IsEnabled = true;
+            if (ViewModel.Data.Speed == MinAttributeValue)
+            {
+                SpeedDownButton.IsEnabled = false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Manage the Attack Up Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void AttackUpButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Attack++;
+
+            if (ViewModel.Data.Attack > MaxAttributeValue)
+            {
+                ViewModel.Data.Attack = MaxAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Manage the Attack Down Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void AttackDownButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Attack--;
+
+            if (ViewModel.Data.Attack < MinAttributeValue)
+            {
+                ViewModel.Data.Attack = MinAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Manage the Defense Up Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DefenseUpButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Defense++;
+
+            if (ViewModel.Data.Defense > MaxAttributeValue)
+            {
+                ViewModel.Data.Defense = MaxAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Manage the Defense Down Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DefenseDownButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Defense--;
+
+            if (ViewModel.Data.Defense < MinAttributeValue)
+            {
+                ViewModel.Data.Defense = MinAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Manage the Speed Up Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void SpeedUpButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Speed++;
+
+            if (ViewModel.Data.Speed > MaxAttributeValue)
+            {
+                ViewModel.Data.Speed = MaxAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Manage the Speed Down Button Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void SpeedDownButton_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.Data.Speed--;
+
+            if (ViewModel.Data.Speed < MinAttributeValue)
+            {
+                ViewModel.Data.Speed = MinAttributeValue;
+            }
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Show the Items the Monster has
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay());
+        }
+
+        /// <summary>
+        /// Select item from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            ViewModel.Data.UniqueItem = data.Id;
+
+            AddItemsToDisplay();
+
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Creates a stack item to return to the flexbox
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay()
+        {
+            var data = ViewModel.Data.GetItem(ViewModel.Data.UniqueItem);
+            if (data == null)
+            {
+                // Show the Default Icon for the Location
+                data = new ItemModel { Location = ItemLocationEnum.Unknown, ImageURI = "icon_cancel.png", Name = "Click to Add" };
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add a event to the user can click the item and see more
+            ItemButton.Clicked += (sender, args) => ShowPopup(data);
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = data.Name,
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+        /// <summary>
+        /// Sets data on the popup page and makes visible
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemModel data)
+        {
+            PopupItemSelector.IsVisible = true;
+
+            // Make a fake item for None
+            var NoneItem = new ItemModel
+            {
+                Id = null, // will use null to clear the item
+                Guid = "None", // how to find this item amoung all of them
+                ImageURI = "icon_cancel.png",
+                Name = "None",
+                Description = "None"
+            };
+
+            List<ItemModel> itemList = new List<ItemModel>
+            {
+                NoneItem
+            };
+
+            // Add the rest of the items to the list
+            itemList.AddRange(ItemIndexViewModel.Instance.Dataset);
+
+            // Populate the list with the items
+            PopupLocationItemListView.ItemsSource = itemList;
+            return true;
+        }
+
+        /// <summary>
+        /// When user click on close, close the popup view and show the scrollview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Close the popup
+        /// </summary>
+        public void ClosePopup()
+        {
+            PopupItemSelector.IsVisible = false;
         }
     }
 }
