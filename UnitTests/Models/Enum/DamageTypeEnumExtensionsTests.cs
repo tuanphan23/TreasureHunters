0fad7e354edfe1be2066a1d91a87a6e2f91b801d
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-02-22 22:27:09 -0800

    Added DamageTypeEnumExtensionsTests.cs file to UnitTestProject with single test

---
+++ UnitTests/Models/Enum/DamageTypeEnumExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class DamageTypeEnumExtensionsTests
+    {
+        [Test]
+        public void DamageTypeEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DamageTypeEnum.None.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("None", result);
+        }
+
+    }
+}
