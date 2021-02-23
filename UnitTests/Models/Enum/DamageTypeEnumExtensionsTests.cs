7443ec543ab934166f75c48c90b2c11b7d1275e0
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-02-22 22:31:05 -0800

    Added the remaining tests for damage types heal, fire, poison, electric to DamageTypeEnumExtensionsTests.cs

--- UnitTests/Models/Enum/DamageTypeEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/DamageTypeEnumExtensionsTests.cs
@@ -8,7 +8,7 @@ namespace UnitTests.Models
     public class DamageTypeEnumExtensionsTests
     {
         [Test]
-        public void DamageTypeEnumExtensionsTests_Unknown_Default_Should_Pass()
+        public void DamageTypeEnumExtensionsTests_None_Default_Should_Pass()
         {
             // Arrange
 
@@ -19,6 +19,62 @@ namespace UnitTests.Models
 
             // Assert
             Assert.AreEqual("None", result);
+        }
+
+        [Test]
+        public void DamageTypeEnumExtensionsTests_Fire_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DamageTypeEnum.Fire.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Fire", result);
+        }
+
+        [Test]
+        public void DamageTypeEnumExtensionsTests_Electric_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DamageTypeEnum.Electric.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Electric", result);
+        }
+
+        [Test]
+        public void DamageTypeEnumExtensionsTests_Poison_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DamageTypeEnum.Poison.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Poison", result);
+        }
+
+        [Test]
+        public void DamageTypeEnumExtensionsTests_Healing_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DamageTypeEnum.Heal.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Healing", result);
         }
 
     }
