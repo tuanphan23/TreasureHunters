046dc363397c74e22609a600acc38acd79f04255
Tuan Phan <tphan@seattleu.edu>
2021-03-07 22:55:59 -0800

    Added UT coverage to BattleEngineViewModelTests.cs

--- UnitTests/ViewModels/BattleEngineViewModelTests.cs
+++ UnitTests/ViewModels/BattleEngineViewModelTests.cs
@@ -53,64 +53,61 @@ namespace UnitTests.ViewModels
             Assert.IsNotNull(result.Engine);
         }
 
-        //this is commented out as BattelEngineViewModel isn't fully implemented
-        //[Test]
-        //public void BattleEngineViewModel_Get_Default_Should_Pass()
-        //{
-        //    // Arrange
+        [Test]
+        public void BattleEngineViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
 
-        //    // Act
-        //    var result = ViewModel;
+            // Act
+            var result = ViewModel;
 
-        //    // Reset
+            // Reset
 
-        //    // Assert
-        //    Assert.IsNotNull(result.DatabaseCharacterList);
-        //    Assert.IsNotNull(null);
-        //    Assert.IsNotNull(result.PartyCharacterList);
-        //}
+            // Assert
+            Assert.IsNotNull(result.DatabaseCharacterList);
+            //Assert.IsNotNull(null);
+            Assert.IsNotNull(result.PartyCharacterList);
+        }
 
-        //this is commented out as BattelEngineViewModel isn't fully implemented
-        //[Test]
-        //public void BattleEngineViewModel_Add_Default_Should_Pass()
-        //{
-        //    // Arrange
-        //    var result = ViewModel;
+        [Test]
+        public void BattleEngineViewModel_Add_Default_Should_Pass()
+        {
+            // Arrange
+            var result = ViewModel;
 
-        //    var countBefore = result.DatabaseCharacterList.Count();
+            var countBefore = result.DatabaseCharacterList.Count();
 
-        //    // Act
-        //    result.DatabaseCharacterList.Add(new CharacterModel());
-        //    result.PartyCharacterList.Add(new CharacterModel());
+            // Act
+            result.DatabaseCharacterList.Add(new CharacterModel());
+            result.PartyCharacterList.Add(new CharacterModel());
 
 
-        //    // Reset
+            // Reset
 
-        //    // Assert
-        //    Assert.AreEqual(countBefore + 1, result.DatabaseCharacterList.Count());
-        //    Assert.AreEqual(1, result.PartyCharacterList.Count());
-        //}
+            // Assert
+            Assert.AreEqual(countBefore + 1, result.DatabaseCharacterList.Count());
+            Assert.AreEqual(1, result.PartyCharacterList.Count());
+        }
 
-        //this is commented out as BattelEngineViewModel isn't fully implemented
-        //[Test]
-        //public void BattleEngineViewModel_Set_Default_Should_Pass()
-        //{
-        //    // Arrange
-        //    var result = ViewModel;
+        [Test]
+        public void BattleEngineViewModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var result = ViewModel;
 
-        //    //var countBefore = result.DatabaseCharacterList.Count();
+            var countBefore = result.DatabaseCharacterList.Count();
 
-        //    // Act
-        //    //result.DatabaseCharacterList = new ObservableCollection<CharacterModel>();
-        //    result.PartyCharacterList = new ObservableCollection<CharacterModel>();
+            // Act
+            result.DatabaseCharacterList = new ObservableCollection<CharacterModel>();
+            result.PartyCharacterList = new ObservableCollection<CharacterModel>();
 
 
-        //    // Reset
+            // Reset
 
-        //    // Assert
-        //    //Assert.AreEqual(0, result.DatabaseCharacterList.Count());
-        //    Assert.AreEqual(0, 1);
-        //    Assert.AreEqual(0, result.PartyCharacterList.Count());
-        //}
+            // Assert
+            Assert.AreEqual(0, result.DatabaseCharacterList.Count());
+            //Assert.AreEqual(0, 1);
+            Assert.AreEqual(0, result.PartyCharacterList.Count());
+        }
     }
 }
