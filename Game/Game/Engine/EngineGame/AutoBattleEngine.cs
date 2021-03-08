e1d27f078af1b7a325cf0e28cd000e34b376d034
Tuan Phan <tphan@seattleu.edu>
2021-03-08 13:27:38 -0800

    Updated CreateCharacterParty in AutoBattleEngine.cs

--- Game/Game/Engine/EngineGame/AutoBattleEngine.cs
+++ Game/Game/Engine/EngineGame/AutoBattleEngine.cs
@@ -1,6 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Game.Engine.EngineBase;
 using Game.Engine.EngineInterfaces;
+using Game.GameRules;
+using Game.Models;
+using Game.Services;
+using Game.ViewModels;
+using Xamarin.Forms;
 
 namespace Game.Engine.EngineGame
 {
@@ -63,8 +70,30 @@ namespace Game.Engine.EngineGame
         /// <returns></returns>
         public override bool CreateCharacterParty()
         {
-            return base.CreateCharacterParty();
-            ////throw new System.NotImplementedException();
+            // Picks 6 Characters
+
+            // To use your own characters, populate the List before calling RunAutoBattle
+
+            //// Will first pull from existing characters
+            foreach (var data in CharacterIndexViewModel.Instance.Dataset)
+            {
+                if (Battle.EngineSettings.CharacterList.Count() >= Battle.EngineSettings.MaxNumberPartyCharacters)
+                {
+                    break;
+                }
+
+                // Start off with max health if adding a character in
+                data.CurrentHealth = data.GetMaxHealthTotal;
+                Battle.PopulateCharacterList(data);
+            }
+
+            //If there are not enough will add random ones
+            for (int i = Battle.EngineSettings.CharacterList.Count(); i < Battle.EngineSettings.MaxNumberPartyCharacters; i++)
+            {
+                Battle.PopulateCharacterList(RandomPlayerHelper.GetRandomCharacter(1));
+            }
+
+            return true;
         }
 
         /// <summary>
