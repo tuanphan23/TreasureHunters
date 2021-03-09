503cb102b3e02ebdd0dafc76a86eef0292c39a41
Tuan Phan <tphan@seattleu.edu>
2021-03-08 22:12:37 -0800

    Added UTs for attack in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -48,5 +48,62 @@ namespace UnitTests.Engine.EngineGame
         }
         #endregion Constructor
 
+        #region Attack
+        [Test]
+        public void TurnEngine_Attack_Valid_Empty_Monster_List_Should_Fail()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel(new CharacterModel());
+
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
+
+            // Reset
+            Engine.StartBattle(false);   // Clear the Engine
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TurnEngine_Attack_InValid_Empty_Character_List_Should_Fail()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel(new MonsterModel());
+
+            // Cause an error by making the list empty
+            Engine.EngineSettings.CharacterList.Clear();
+            Engine.EngineSettings.CurrentDefender = null;
+            Engine.EngineSettings.CurrentAttacker = null;
+
+            Engine.StartBattle(true);   // Clear the Engine
+
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
+
+            // Reset
+            Engine.StartBattle(false);   // Clear the Engine
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void TurnEngine_Attack_Valid_Correct_List_Should_Pass()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel();
+            Engine.EngineSettings.MonsterList.Add(new PlayerInfoModel(new MonsterModel()));
+
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
+
+            // Reset
+            Engine.StartBattle(false);   // Clear the Engine
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+        #endregion Attack
     }
 }
