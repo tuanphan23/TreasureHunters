d781b66be6a7370a89dd5743304e389a972791cf
Nicholas Doubrovsky <doubrovs@seattleu.edu>
2021-03-08 17:45:43 -0800

    fixed a numerical tyoi default round engine player list length should be 6 in RoundEngineBaseTests.cs

--- UnitTests/Engine/EngineBase/RoundEngineBaseTests.cs
+++ UnitTests/Engine/EngineBase/RoundEngineBaseTests.cs
@@ -1172,7 +1172,7 @@ namespace UnitTests.Engine.EngineBase
             // Reset
 
             // Assert
-            Assert.AreEqual(7, result.Count);
+            Assert.AreEqual(6, result.Count);
         }
         #endregion PlayerList
 
