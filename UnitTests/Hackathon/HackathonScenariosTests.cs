12375058c9714513ae4627d94f07d9ac48a3ab03
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-03-10 17:36:42 -0800

    Added condition for timewarp to RoundEngine.cs

--- UnitTests/Hackathon/HackathonScenariosTests.cs
+++ UnitTests/Hackathon/HackathonScenariosTests.cs
@@ -152,5 +152,97 @@ namespace Scenario
             Assert.AreEqual(1, EngineViewModel.Engine.EngineSettings.BattleScore.RoundCount);
         }
         #endregion Scenario1
+
+        #region Scenario16
+
+        [Test]
+        public async Task HackathonScenario_Scenario_16_ToggleActive_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      16
+            *      
+            * Description: 
+            *      Sets toggle for the characters to be ordered by speed starting with the slowest. 
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      Changes to the RoundEngineBase.cs - OrderPlayerListByTurnOrder()
+            *      Changes to add toggle to the BattlePage.xaml and the corresponding function call
+            *      in the code behind as well as the BattleSettingsModel.cs adding the TimeWarp variable
+            * 
+            * Test Algrorithm:
+            *      Create Character named Mike
+            *      Create Character named Doug
+            *      Set speed to -1 so he is really slow
+            *      Set Doug Speed to 10 so hes fast
+            *    
+            *      Mike should be first on the list at end of round
+            *  
+            *      Startup Battle
+            *      Run Auto Battle
+            *      
+            *      Check lsit
+            * 
+            * Test Conditions:
+            *      Default condition is sufficient
+            * 
+            * Validation:
+            *      Verify Battle Returned True
+            *      Verify Mike is first player in the list
+            *      Verify Round Count is 1
+            *  
+            */
+
+            //Arrange
+
+            // Set Character Conditions
+
+            EngineViewModel.EngineGame.EngineSettings.BattleSettingsModel.TimeWarp = true;
+
+            var CharacterPlayerMike = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = -1, // Will go first...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Mike",
+                            });
+
+            var CharacterPlayerDoug = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = 10, // Will go last...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Doug",
+                            });
+
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayerMike);
+
+            // Set Monster Conditions
+
+            // Auto Battle will add the monsters
+
+            // Monsters always hit so round ends
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Hit;
+
+            //Act
+            var result = await EngineViewModel.AutoBattleEngine.RunAutoBattle();
+
+            //Reset
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
+
+            //Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(CharacterPlayerMike, EngineViewModel.Engine.EngineSettings.PlayerList[0]);
+            Assert.AreEqual(1, EngineViewModel.Engine.EngineSettings.BattleScore.RoundCount);
+        }
+
+
+        #endregion Scenario16
     }
 }
