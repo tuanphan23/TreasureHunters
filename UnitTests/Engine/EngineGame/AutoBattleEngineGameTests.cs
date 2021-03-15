6b5a50d4b9481a2ca45ac6efcfaf5bb9b8583fd0
Tuan Phan <tphan@seattleu.edu>
2021-03-15 15:24:24 -0700

    Added UTs for AutoBattleEngineGameTests.cs

--- UnitTests/Engine/EngineGame/AutoBattleEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/AutoBattleEngineGameTests.cs
@@ -17,23 +17,19 @@ namespace UnitTests.Engine.EngineGame
     public class AutoBattleEngineGameTests
     {
         #region TestSetup
-        AutoBattleEngine AutoBattleEngine;
+        public AutoBattleEngine Engine;
 
         [SetUp]
         public void Setup()
         {
-            AutoBattleEngine = new AutoBattleEngine();
+            Engine = new AutoBattleEngine();
 
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Clear();
-            AutoBattleEngine.Battle.EngineSettings.MonsterList.Clear();
-            AutoBattleEngine.Battle.EngineSettings.CurrentDefender = null;
-            AutoBattleEngine.Battle.EngineSettings.CurrentAttacker = null;
+            Engine.Battle.EngineSettings.CharacterList.Clear();
+            Engine.Battle.EngineSettings.MonsterList.Clear();
+            Engine.Battle.EngineSettings.CurrentDefender = null;
+            Engine.Battle.EngineSettings.CurrentAttacker = null;
 
-            AutoBattleEngine.Battle.Round = new RoundEngine();
-            AutoBattleEngine.Battle.Round.Turn = new TurnEngine();
-
-            // Remove this comment when the Battle engine is ready to go...
-            //AutoBattleEngine.Battle.StartBattle(true);   // Clear the Engine
+            Engine.Battle.StartBattle(true);   // Clear the Engine
         }
 
         [TearDown]
@@ -49,13 +45,31 @@ namespace UnitTests.Engine.EngineGame
             // Arrange
 
             // Act
-            var result = AutoBattleEngine;
+            var result = Engine;
 
             // Reset
 
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void AutoBattleEngine_Constructor_Valid_Battle_Round_Turn_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine;
+            result.Battle = new BattleEngine();
+            result.Battle.Round = new RoundEngine();
+            result.Battle.Round.Turn = new TurnEngine();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
         #endregion Constructor
 
         #region RunAutoBattle
@@ -69,15 +83,15 @@ namespace UnitTests.Engine.EngineGame
 
             var data = new CharacterModel { Level = 1, MaxHealth = 10 };
 
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
 
             //Act
-            var result = await AutoBattleEngine.RunAutoBattle();
+            var result = await Engine.RunAutoBattle();
 
             //Reset
             DiceHelper.DisableForcedRolls();
@@ -93,8 +107,8 @@ namespace UnitTests.Engine.EngineGame
             //Arrange
 
             // Need to set the Monster count to 1, so the battle goes to Next Round Faster
-            AutoBattleEngine.Battle.EngineSettings.MaxNumberPartyMonsters = 1;
-            AutoBattleEngine.Battle.EngineSettings.MaxNumberPartyCharacters = 1;
+            Engine.Battle.EngineSettings.MaxNumberPartyMonsters = 1;
+            Engine.Battle.EngineSettings.MaxNumberPartyCharacters = 1;
 
             var CharacterPlayerMike = new PlayerInfoModel(
                             new CharacterModel
@@ -108,10 +122,42 @@ namespace UnitTests.Engine.EngineGame
                                 ListOrder = 1,
                             });
 
-            AutoBattleEngine.Battle.EngineSettings.CharacterList.Add(CharacterPlayerMike);
+            Engine.Battle.EngineSettings.CharacterList.Add(CharacterPlayerMike);
 
             //Act
-            var result = await AutoBattleEngine.RunAutoBattle();
+            var result = await Engine.RunAutoBattle();
+
+            //Reset
+            CharacterIndexViewModel.Instance.ForceDataRefresh();
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public async Task AutoBattleEngine_RunAutoBattle_Valid_Monster_1_Should_ReturnNewRound()
+        {
+            //Arrange
+
+            // Need to set the Monster count to 1, so the battle goes to Next Round Faster
+            Engine.Battle.EngineSettings.MaxNumberPartyMonsters = 1;
+            Engine.Battle.EngineSettings.MaxNumberPartyCharacters = 2;
+
+            // Make a Strong Character with lots of health,  set level to 1 so the monsters are low level
+            Engine.Battle.EngineSettings.CharacterList.Add(new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = 100,
+                                Level = 1,
+                                CurrentHealth = 110,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Mike",
+                                ListOrder = 1,
+                            }));
+
+            //Act
+            var result = await Engine.RunAutoBattle();
 
             //Reset
             CharacterIndexViewModel.Instance.ForceDataRefresh();
@@ -126,7 +172,7 @@ namespace UnitTests.Engine.EngineGame
         public async Task AutoBattleEngine_CreateCharacterParty_Valid_Characters_Should_Assign_6()
         {
             //Arrange
-            AutoBattleEngine.Battle.EngineSettings.MaxNumberPartyCharacters = 6;
+            Engine.Battle.EngineSettings.MaxNumberPartyCharacters = 6;
 
             CharacterIndexViewModel.Instance.Dataset.Clear();
 
@@ -139,9 +185,9 @@ namespace UnitTests.Engine.EngineGame
             await CharacterIndexViewModel.Instance.CreateAsync(new CharacterModel { Name = "7" });
 
             //Act
-            var result = AutoBattleEngine.CreateCharacterParty();
-            var count = AutoBattleEngine.Battle.EngineSettings.CharacterList.Count();
-            var name = AutoBattleEngine.Battle.EngineSettings.CharacterList.ElementAt(5).Name;
+            var result = Engine.CreateCharacterParty();
+            var count = Engine.Battle.EngineSettings.CharacterList.Count();
+            var name = Engine.Battle.EngineSettings.CharacterList.ElementAt(5).Name;
 
             //Reset
             CharacterIndexViewModel.Instance.ForceDataRefresh();
@@ -155,13 +201,13 @@ namespace UnitTests.Engine.EngineGame
         public void AutoBattleEngine_CreateCharacterParty_Valid_Characters_CharacterIndex_None_Should_Create_6()
         {
             //Arrange
-            AutoBattleEngine.Battle.EngineSettings.MaxNumberPartyCharacters = 6;
+            Engine.Battle.EngineSettings.MaxNumberPartyCharacters = 6;
 
             CharacterIndexViewModel.Instance.Dataset.Clear();
 
             //Act
-            var result = AutoBattleEngine.CreateCharacterParty();
-            var count = AutoBattleEngine.Battle.EngineSettings.CharacterList.Count();
+            var result = Engine.CreateCharacterParty();
+            var count = Engine.Battle.EngineSettings.CharacterList.Count();
 
             //Reset
             CharacterIndexViewModel.Instance.ForceDataRefresh();
@@ -170,5 +216,90 @@ namespace UnitTests.Engine.EngineGame
             Assert.AreEqual(6, count);
         }
         #endregion CreateCharacterParty   
+
+        #region DetectInfinateLoop
+
+        [Test]
+        public void AutoBattleEngine_DetectInfinateLoop_InValid_RoundCount_Higher_MaxRound_Should_Return_True()
+        {
+            //Arrange
+            var oldTurnCount = Engine.Battle.EngineSettings.BattleScore.TurnCount;
+            var oldTurnMax = Engine.Battle.EngineSettings.MaxTurnCount;
+            var oldRoundCount = Engine.Battle.EngineSettings.BattleScore.RoundCount;
+            var oldRoundMax = Engine.Battle.EngineSettings.MaxRoundCount;
+
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = 10;
+            Engine.Battle.EngineSettings.MaxRoundCount = 1;
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = 1;
+            Engine.Battle.EngineSettings.MaxTurnCount = 10;
+
+            //Act
+            var result = Engine.DetectInfinateLoop();
+
+            //Reset
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = oldTurnCount;
+            Engine.Battle.EngineSettings.MaxTurnCount = oldTurnMax;
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = oldRoundCount;
+            Engine.Battle.EngineSettings.MaxRoundCount = oldRoundMax;
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_DetectInfinateLoop_InValid_TurnCount_Higher_MaxTurn_Should_Return_True()
+        {
+            //Arrange
+            var oldTurnCount = Engine.Battle.EngineSettings.BattleScore.TurnCount;
+            var oldTurnMax = Engine.Battle.EngineSettings.MaxTurnCount;
+            var oldRoundCount = Engine.Battle.EngineSettings.BattleScore.RoundCount;
+            var oldRoundMax = Engine.Battle.EngineSettings.MaxRoundCount;
+
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = 1;
+            Engine.Battle.EngineSettings.MaxRoundCount = 10;
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = 10;
+            Engine.Battle.EngineSettings.MaxTurnCount = 1;
+
+            //Act
+            var result = Engine.DetectInfinateLoop();
+
+            //Reset
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = oldTurnCount;
+            Engine.Battle.EngineSettings.MaxTurnCount = oldTurnMax;
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = oldRoundCount;
+            Engine.Battle.EngineSettings.MaxRoundCount = oldRoundMax;
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_DetectInfinateLoop_Valid_TurnCount__RoundCount_Lower_Max_Should_Return_false()
+        {
+            //Arrange
+            var oldTurnCount = Engine.Battle.EngineSettings.BattleScore.TurnCount;
+            var oldTurnMax = Engine.Battle.EngineSettings.MaxTurnCount;
+            var oldRoundCount = Engine.Battle.EngineSettings.BattleScore.RoundCount;
+            var oldRoundMax = Engine.Battle.EngineSettings.MaxRoundCount;
+
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = 1;
+            Engine.Battle.EngineSettings.MaxRoundCount = 10;
+
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = 1;
+            Engine.Battle.EngineSettings.MaxTurnCount = 10;
+
+            //Act
+            var result = Engine.DetectInfinateLoop();
+
+            //Reset
+            Engine.Battle.EngineSettings.BattleScore.TurnCount = oldTurnCount;
+            Engine.Battle.EngineSettings.MaxTurnCount = oldTurnMax;
+            Engine.Battle.EngineSettings.BattleScore.RoundCount = oldRoundCount;
+            Engine.Battle.EngineSettings.MaxRoundCount = oldRoundMax;
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+        #endregion DetectInfinateLoop   
     }
 }
