84c98fbb44646fd060d93c425ba8c5386ca3cac7
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-03-15 17:50:26 -0700

    Updated the package manifest and assembly info for UWP project

--- Game/Game.UWP/Properties/AssemblyInfo.cs
+++ Game/Game.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Elves vs Trolls")]
-[assembly: AssemblyDescription("Mike's Cool Game")]
+[assembly: AssemblyTitle("National Treasure 3")]
+[assembly: AssemblyDescription("Dungeon Crawler by Treasure Hunters")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SU")]
-[assembly: AssemblyProduct("Elves vs Trolls")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Seattle University")]
+[assembly: AssemblyProduct("National Treasure Game")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
