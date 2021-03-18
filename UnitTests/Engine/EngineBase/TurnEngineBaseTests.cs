2534d98b2c8a99e559074933b19583a2933f3d61
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-03-17 20:13:02 -0700

    Added TurnEngine_ChooseToUseAbility_InValid_Roll_2_Should_Return_True() to TrunEngineBaseTests.cs

--- UnitTests/Engine/EngineBase/TurnEngineBaseTests.cs
+++ UnitTests/Engine/EngineBase/TurnEngineBaseTests.cs
@@ -1452,6 +1452,38 @@ namespace UnitTests.Engine.EngineBase
         }
 
         [Test]
+        public void TurnEngine_ChooseToUseAbility_InValid_Roll_2_Should_Return_True()
+        {
+            // Arrange
+
+            var CharacterPlayer = new PlayerInfoModel(new CharacterModel());
+            CharacterPlayer.CurrentHealth = 1;
+            CharacterPlayer.MaxHealth = 100;
+            // Get the item with heal ability in stock.
+            var weapon = ItemIndexViewModel.Instance.Dataset.Where(m => m.DamageType == DamageTypeEnum.Heal).ToList().OrderByDescending(m => m.Value).FirstOrDefault();
+            CharacterPlayer.OffHand = weapon.Id;
+
+            Engine.EngineSettings.PlayerList.Add(CharacterPlayer);
+
+            Engine.EngineSettings.MapModel.PopulateMapModel(Engine.EngineSettings.PlayerList);
+
+            Engine.EngineSettings.CurrentAction = ActionEnum.Unknown;
+            Engine.EngineSettings.BattleScore.AutoBattle = false;
+
+            DiceHelper.EnableForcedRolls();
+            DiceHelper.SetForcedRollValue(2);
+            // Act
+            var result = Engine.Round.Turn.ChooseToUseAbility(CharacterPlayer);
+
+            // Reset
+            DiceHelper.DisableForcedRolls();
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+
+        [Test]
         public void TurnEngine_ChooseToUseAbility_InValid_Roll_2_No_Ability_Should_Return_False()
         {
             // Arrange
